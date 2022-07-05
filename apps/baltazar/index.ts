c9c7a59cd26d72a44e13917164096b756e5973ab
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-07-05 19:56:05 -0300

    Feat(vercel):Added serveless function

--- apps/baltazar/index.ts
+++ apps/baltazar/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-
+import apiTest from "./api";
 dotenv.config({ path: "../../.env" });
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.json({ status: new Date() });
+  apiTest(req, res);
 });
-
 // start the Express server
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
